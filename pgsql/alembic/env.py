76f5a5803bc529506283411d71553fde29327b22
Jacques Arnoux <jacques@dioptra.ai>
2022-08-07 17:08:16 +0200

    Added alembic upgrade in circle-ci

--- pgsql/alembic/env.py
+++ pgsql/alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -8,6 +9,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+portgres_url = os.environ.get("POSTGRES_URL", "postgresql://dioptra:dioptraai@localhost/dioptra")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -38,9 +40,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=portgres_url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -57,8 +58,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    config_section = config.get_section(config.config_ini_section)
+    config_section["sqlalchemy.url"] = portgres_url
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config_section,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
