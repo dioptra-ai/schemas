b26ee7dbb3f1fd81ec53758010c7996072432af2
Jacques Arnoux <jacques@dioptra.ai>
2022-08-11 14:41:43 +0200

    removed unncessary arg

--- pgsql/models/event.py
+++ pgsql/models/event.py
@@ -26,7 +26,7 @@ class Event(Base):
     model_type = Column(String())
     input_type = Column(String())
 
-    tags = Column('tags', JSONB())
+    tags = Column(JSONB())
     features = Column(JSONB())
     image_metadata = Column(JSONB())
     video_metadata = Column(JSONB())
