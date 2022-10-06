a1d2b91cf57dceee1fb70bcbbaa0f093da08439d
Jacques Arnoux <jacques@dioptra.ai>
2022-10-06 13:42:21 -0700

    trying to solve the SSL issue again

--- pgsql/__init__.py
+++ pgsql/__init__.py
@@ -8,18 +8,17 @@ POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
 POSTGRES_PORT = os.environ.get('POSTGRES_PORT', 5432)
 POSTGRES_DATABASE = os.environ.get('POSTGRES_DATABASE', 'dioptra')
 
-engine = create_engine(
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
-    # Outputs all SQL queries for debugging.
-    # To log queries in consumer applications instead of using this, use the following:
-    # @see https://docs.sqlalchemy.org/en/14/core/engines.html#configuring-logging
-    # echo=True,
-    future=True,
-    pool_size=1024,
-    max_overflow=1024,
-)
-
 def get_session():
-    engine.dispose(close=False)
 
-    return Session(engine)
+    return Session(
+        create_engine(
+            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
+            # Outputs all SQL queries for debugging.
+            # To log queries in consumer applications instead of using this, use the following:
+            # @see https://docs.sqlalchemy.org/en/14/core/engines.html#configuring-logging
+            # echo=True,
+            future=True,
+            pool_size=1024,
+            max_overflow=1024,
+        )
+    )
