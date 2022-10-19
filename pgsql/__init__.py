db9db845ff485f6298211c682a5237baceb39bee
Jacques Arnoux <jacques@dioptra.ai>
2022-10-19 16:09:10 -0700

    trying a new simpler connection method to fix the ssl issue in metrics engine

--- pgsql/__init__.py
+++ pgsql/__init__.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
@@ -14,12 +14,21 @@ def get_session():
     return Session(
         create_engine(
             f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
-            # Outputs all SQL queries for debugging.
-            # To log queries in consumer applications instead of using this, use the following:
-            # @see https://docs.sqlalchemy.org/en/14/core/engines.html#configuring-logging
             # echo=True,
             future=True,
             pool_size=POSTGRES_MAX_CONNECTIONS / 2,
             max_overflow=POSTGRES_MAX_CONNECTIONS / 2,
         )
     )
+
+def run_sql_query(sql_query):
+    engine = create_engine(
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
+        # echo=True,
+        future=True,
+        pool_size=POSTGRES_MAX_CONNECTIONS / 2,
+        max_overflow=POSTGRES_MAX_CONNECTIONS / 2,
+    )
+
+    with engine.connect() as conn:
+        return conn.execute(text(sql_query))
