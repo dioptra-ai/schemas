b2a89059e3b5bb3acbf8c9a2860ec16b686a70c0
Jacques Arnoux <jacques@dioptra.ai>
2022-11-11 20:26:03 -0800

    now reusing the same sql_engine for postgres

--- pgsql/__init__.py
+++ pgsql/__init__.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, event, exc
 from sqlalchemy.orm import Session
 
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
@@ -9,36 +9,37 @@ POSTGRES_PORT = os.environ.get('POSTGRES_PORT', 5432)
 POSTGRES_DATABASE = os.environ.get('POSTGRES_DATABASE', 'dioptra')
 POSTGRES_MAX_CONNECTIONS = int(os.environ.get('POSTGRES_MAX_CONNECTIONS', 2048))
 
-def get_session():
+_sql_engine = create_engine(
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
+    # echo=True,
+    future=True,
+    pool_size=POSTGRES_MAX_CONNECTIONS / 2,
+    max_overflow=POSTGRES_MAX_CONNECTIONS / 2,
+)
 
-    return Session(
-        create_engine(
-            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
-            # echo=True,
-            future=True,
-            pool_size=POSTGRES_MAX_CONNECTIONS / 2,
-            max_overflow=POSTGRES_MAX_CONNECTIONS / 2,
+@event.listens_for(_sql_engine, "connect")
+def connect(dbapi_connection, connection_record):
+    connection_record.info["pid"] = os.getpid()
+
+@event.listens_for(_sql_engine, "checkout")
+def checkout(dbapi_connection, connection_record, connection_proxy):
+    pid = os.getpid()
+    if connection_record.info["pid"] != pid:
+        connection_record.dbapi_connection = connection_proxy.dbapi_connection = None
+        raise exc.DisconnectionError(
+            "Connection record belongs to pid %s, "
+            "attempting to check out in pid %s" % (connection_record.info["pid"], pid)
         )
-    )
-
-def run_sql_query(sql_query):
-    engine = create_engine(
-        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
-        # echo=True,
-        future=True,
-        pool_size=POSTGRES_MAX_CONNECTIONS / 2,
-        max_overflow=POSTGRES_MAX_CONNECTIONS / 2,
-    )
-
-    with engine.connect() as conn:
-        return conn.execute(text(sql_query))
 
 def get_sql_engine():
 
-    return create_engine(
-        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}",
-        # echo=True,
-        future=True,
-        pool_size=POSTGRES_MAX_CONNECTIONS / 2,
-        max_overflow=POSTGRES_MAX_CONNECTIONS / 2,
-    )
+    return _sql_engine
+
+def get_session():
+
+    return Session(get_sql_engine())
+
+def run_sql_query(sql_query):
+
+    with get_sql_engine().connect() as conn:
+        return conn.execute(text(sql_query))
