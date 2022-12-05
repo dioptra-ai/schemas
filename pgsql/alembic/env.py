1f237adcac8cefd086cefe23a1ed31d3561e4d53
Jacques Arnoux <jacques@dioptra.ai>
2022-12-04 23:31:22 -0800

    ignoring celery tables

--- pgsql/alembic/env.py
+++ pgsql/alembic/env.py
@@ -30,15 +30,16 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 from models import Base
 target_metadata = Base.metadata
-# target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 from functions import functions
 register_entities(functions)
+
+def include_name(name, type_, parent_names):
+    if type_ == "table":
+        # Excluded tables.
+        return name not in ["celery_tasksetmeta", "celery_taskmeta"]
+    else:
+        return True
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -57,6 +58,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_name=include_name
     )
 
     with context.begin_transaction():
@@ -80,7 +82,9 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, 
+            target_metadata=target_metadata,
+            include_name=include_name
         )
 
         with context.begin_transaction():
