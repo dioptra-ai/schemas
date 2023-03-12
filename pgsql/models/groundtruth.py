1907055520dad877f35d7fe9938ae386705e9f4f
Pierre <pierre@dioptra.ai>
2023-03-12 16:18:10 -0400

    Adding encoded segmentation mask

--- pgsql/models/groundtruth.py
+++ pgsql/models/groundtruth.py
@@ -23,6 +23,7 @@ class GroundTruth(Base):
     class_name = Column(String(), nullable=True)
     class_names = Column(ARRAY(String()), nullable=True)
     segmentation_class_mask = Column(JSONB, nullable=True)
+    encoded_segmentation_class_mask = Column(String(), nullable=True)
     top = Column(Float(), nullable=True)
     left = Column(Float(), nullable=True)
     height = Column(Float(), nullable=True)
