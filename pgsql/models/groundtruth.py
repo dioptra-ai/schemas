22a49483f7e241f518686e222f8dbe7e5cc855ac
Jacques Arnoux <jacques@dioptra.ai>
2023-03-16 20:26:35 -0700

    adds metrics column to groundtruths

--- pgsql/models/groundtruth.py
+++ pgsql/models/groundtruth.py
@@ -28,6 +28,7 @@ class GroundTruth(Base):
     left = Column(Float(), nullable=True)
     height = Column(Float(), nullable=True)
     width = Column(Float(), nullable=True)
+    metrics = Column(JSONB, nullable=True)
 
     def __repr__(self):
         return f"GroundTruth(id={self.id!r}, datapoint={self.datapoint!r})"
